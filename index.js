d399f9102e2f516610b8171d85aa881b42cf75dd
Carol Pascott <65988152+carolpascott@users.noreply.github.com>
2022-06-19 20:07:10 -0300

    Add files via upload
    projeto finalizado

--- index.js
+++ index.js
@@ -1,32 +1,51 @@
-/* busca pelo id=resposta no doc html*/
-const elementResposta = document.querySelector("#resposta")
-const elementPergunta = document.querySelector("#pergunta")
+const respostas = [
+"certeza",
+"não tenho tanta certeza",
+"é decididamente assim",
+"não conte com isso",
+"sem dúvidas",
+"pergunte novamente mais tarde",
+"sim, definitivamente",
+"minha resposta é não",
+"você pode contar com isso",
+"melhor não te dizer agora",
+"a meu ver, sim",
+"minhas fontes dizem não",
+"provavelmente",
+"não é possível prever agora",
+"prespectiva boa",
+"as prespectivas não são tão boas",
+"sim",
+"concentre-se e pergunte novamente",
+"sinais apontam que sim"	
+]
 
-function pergunta() {
-	if(elementPergunta.value == "") {
+// busca pelo id=resposta no doc html
+const respostaElement = document.querySelector("#resposta")
+const perguntaElement = document.querySelector("#pergunta")
+const botaoElement	= document.querySelector("#botao") 
+
+function fazerPergunta() {
+	
+	if(perguntaElement.value == "") {
 		alert("Digite sua pergunta!")
 		return
 	}
-	else
-		respostaPergunta()
-}
+	botaoElement.setAttribute("disabled", true)
 
-
-function respostaPergunta() {
-	
-	const respostas = [
-	"certeza",
-	"não tenho tanta certeza",
-	"é decididamente assim",
-	"não conte com isso",
-	"sem dúvidas",
-	"pergunte novamente mais tarde",	
-	]
-
+	// gerar num aleatorio
 	const totalRespostas = respostas.length;
 	const numAleatorio = Math.floor(Math.random() * totalRespostas)
+	// variavel com a pergunta feita dentro de uma <div> (html)
+	const pergunta = "<div>" + perguntaElement.value + "</div>"
+
+	// innerHTML => mostrar a resposta no #resposta(h3 no html)
+	respostaElement.style.opacity = 1;
+	respostaElement.innerHTML = pergunta + respostas[numAleatorio]
 	
-	/* innerHTML => mostrar a resposta no #resposta(h3 no html)*/
-	elementResposta.innerHTML = respostas[numAleatorio]
-	
+	// sumir resposta apos 3s e habilita novamente botao
+	setTimeout(function() {
+		respostaElement.style.opacity = 0;
+		botaoElement.removeAttribute("disabled")
+	}, 3000)	
 }
